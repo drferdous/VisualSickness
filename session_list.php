d90f03394301b26447a1e705b5c8cb3e0fae80ce
Kyla <57500265+kyla0509@users.noreply.github.com>
2021-07-14 15:33:10 -0400

    Added name & start time to session list

--- session_list.php
+++ session_list.php
@@ -18,8 +18,7 @@ Session::CheckSession();
         
     <div class="card-body pr-2 pl-2">
     <?php
-        $sql = "SELECT session_ID FROM Session WHERE study_ID = " . $_GET["study_ID"];
-        
+        $sql = "SELECT session_ID, start_time, participant_ID FROM Session WHERE study_ID = " . $_GET["study_ID"];
         $result = mysqli_query($conn, $sql);
             
         if (mysqli_num_rows($result) > 0){
@@ -29,17 +28,32 @@ Session::CheckSession();
                 <thead class="text-center">
                     <tr>
                         <th>Session ID</th>
+                        <th>Participant Name</th>
+                        <th>Start Time</th>                        
                         <th>Action</th>
                     </tr>
                 </thead>
                     
                 <tbody>
+        
                 <?php
                     while ($row = mysqli_fetch_assoc($result)) { 
                         echo "<tr>";
                         
                         echo "<td>" . $row['session_ID'] ."</td>";
                         
+                        if (isset($row['participant_ID'])){
+                            $sql_users = "SELECT anonymous_name FROM Participant WHERE participant_id = " . $row['participant_ID'] . " LIMIT 1;";
+                            $result_users = mysqli_query($conn, $sql_users);
+                            $row_users = mysqli_fetch_assoc($result_users);
+
+                            echo "<td>" . $row_users['anonymous_name'] . "</td>";
+                        } else {
+                            echo "<td>-</td>";
+                        }                             
+
+                        echo "<td>" .  $row['start_time']     . "</td>";                           
+                    
                         echo "<td>";
                         echo "<a class='btn-success btn-sm' href=\"session_details.php?session_ID=" . $row['session_ID'] . "\">Session Details</a>";
                         
