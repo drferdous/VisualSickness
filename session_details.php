9f58024f5792726bf1df32d7e9a84bcba03c06fa
Kyla <57500265+kyla0509@users.noreply.github.com>
2021-07-14 14:59:19 -0400

    Session details displaysinformation vertically in a column

--- session_details.php
+++ session_details.php
@@ -41,91 +41,124 @@ if (isset($_POST['end-session-btn'])){
             <thead class="text-center">
                 <tr>
                     <th>Session ID</th>
-                    <th>Study Name</th>
-                    <th>Participant Name</th>           
-                    <th># of Quizzes Taken</th>                       
-                    <th>Start Time</th>
-                    <th>End Time</th>
-                    <th>Comment</th>
-                    <th>Created By</th>
-                    <th>Last Edited By</th>    
-                    <th>Action</th>
-                </tr>
-            </thead>
-            
-            <tbody>
+                        <?php
+                        $sql_session = "SELECT * FROM Session WHERE session_ID = " . $_GET["session_ID"];
+                        $mysqli_result = mysqli_query($conn, $sql_session);
+                        $row_session = mysqli_fetch_assoc($mysqli_result);
+                        echo "<td>" .  $row_session['session_ID']  . "</td>";     
+                        ?>
+                </tr> 
+                
                 <tr>
-                <?php
-                $sql_session = "SELECT * FROM Session WHERE session_ID = " . $_GET["session_ID"];
-                $mysqli_result = mysqli_query($conn, $sql_session);
-                $row_session = mysqli_fetch_assoc($mysqli_result);
-                echo "<td>" .  $row_session['session_ID']  . "</td>";
+                    <th>Study Name</th>
+                        <?php
+                        // show name for study_ID, not id         
+                        if (isset($row_session['study_ID'])){
+                            $sql_users = "SELECT full_name FROM Study WHERE study_id = " . $row_session['study_ID'] . " LIMIT 1;";
+                            $result_users = mysqli_query($conn, $sql_users);
+                            $row_users = mysqli_fetch_assoc($result_users);
+                                
+                            echo "<td>" . $row_users['full_name'] . "</td>";
+                        }
+                        else{
+                            echo "<td>-</td>";
+                        }
+                        ?>
+                </tr>  
                 
-                // show name for participant_ID, not id         
-                if (isset($row_session['study_ID'])){
-                    $sql_users = "SELECT full_name FROM Study WHERE study_id = " . $row_session['study_ID'] . " LIMIT 1;";
-                    $result_users = mysqli_query($conn, $sql_users);
-                    $row_users = mysqli_fetch_assoc($result_users);
-                        
-                    echo "<td>" . $row_users['full_name'] . "</td>";
-                }
-                else{
-                    echo "<td>-</td>";
-                }                    
+                <tr>
+                    <th>Participant Name</th>   
+                        <?php
+                        // show name for participant_ID, not id         
+                        if (isset($row_session['participant_ID'])){
+                            $sql_users = "SELECT anonymous_name FROM Participant WHERE participant_id = " . $row_session['participant_ID'] . " LIMIT 1;";
+                            $result_users = mysqli_query($conn, $sql_users);
+                            $row_users = mysqli_fetch_assoc($result_users);
+                                
+                            echo "<td>" . $row_users['anonymous_name'] . "</td>";
+                        }
+                        else{
+                            echo "<td>-</td>";
+                        }     
+                        ?>
+                    </tr>
+                    
+                <tr>  
+                    <th># of Quizzes Taken</th>   
+                    <?php
+                    echo "<td>" .  $row_session['quizzes_taken']     . "</td>";
+                    ?>
+                </tr> 
                 
-                // show name for participant_ID, not id         
-                if (isset($row_session['participant_ID'])){
-                    $sql_users = "SELECT anonymous_name FROM Participant WHERE participant_id = " . $row_session['participant_ID'] . " LIMIT 1;";
-                    $result_users = mysqli_query($conn, $sql_users);
-                    $row_users = mysqli_fetch_assoc($result_users);
-                        
-                    echo "<td>" . $row_users['anonymous_name'] . "</td>";
-                }
-                else{
-                    echo "<td>-</td>";
-                }                
-
-                echo "<td>" .  $row_session['quizzes_taken']     . "</td>";
-                echo "<td>" .  $row_session['start_time']     . "</td>";
-                echo "<td>" .  $row_session['end_time'] . "</td>";
-                echo "<td>" .  $row_session['comment'] . "</td>";
-                
-                // show name for created_by, not id         
-                if (isset($row_session['created_by'])){
-                    $sql_users = "SELECT name FROM tbl_users WHERE id = " . $row_session['created_by'] . " LIMIT 1;";
-                    $result_users = mysqli_query($conn, $sql_users);
-                    $row_users = mysqli_fetch_assoc($result_users);
-                        
-                    echo "<td>" . $row_users['name'] . "</td>";
-                } else{
-                    echo "<td>-</td>";
-                }
-                
-                // show name for last_edited_by, not id    
-                if (isset($row_session['last_edited_by'])){
-                    $sql_users = "SELECT name FROM tbl_users WHERE id = " . $row_session['last_edited_by'] . " LIMIT 1;";
-                    $result_users = mysqli_query($conn, $sql_users);
-                    $row_users = mysqli_fetch_assoc($result_users);
-                    echo "<td>" . $row_users['name'] . "</td>";
-                } else {
-                    echo "<td>-</td>";
-                }                
-                
-                
-                echo "<td>";
-                echo "<form method=\"post\">";
-                if (isset($row_session['end_time'])){
-                    echo "<input type=\"submit\" name=\"restart-session-btn\" value=\"Restart Session\">";
-                }
-                else{
-                    echo "<input type=\"submit\" name=\"end-session-btn\"   value=\"End Session\">";
-                }
-                echo "</form>";
-                echo "</td>";
-                ?> 
+                <tr>
+                    <th>Start Time</th>
+                    <?php
+                    echo "<td>" .  $row_session['start_time']     . "</td>";       
+                    ?>
                 </tr>
-            </tbody>
-            
+                
+                <tr>
+                    <th>End Time</th>
+                    <?php
+                    echo "<td>" .  $row_session['end_time'] . "</td>";
+                    ?>
+                </tr>
+                
+                <tr>
+                    <th>Comment</th>
+                    <?php
+                    echo "<td>" .  $row_session['comment'] . "</td>";    
+                    ?>
+                </tr>
+                
+                <tr>
+                    <th>Created By</th>
+                    <?php
+                    // show name for created_by, not id         
+                    if (isset($row_session['created_by'])){
+                        $sql_users = "SELECT name FROM tbl_users WHERE id = " . $row_session['created_by'] . " LIMIT 1;";
+                        $result_users = mysqli_query($conn, $sql_users);
+                        $row_users = mysqli_fetch_assoc($result_users);
+                            
+                        echo "<td>" . $row_users['name'] . "</td>";
+                    } else{
+                        echo "<td>-</td>";
+                    }       
+                    ?>
+                </tr>   
+                
+                <tr>
+                    <th>Last Edited By</th>  
+                    <?php
+                    // show name for last_edited_by, not id    
+                    if (isset($row_session['last_edited_by'])){
+                        $sql_users = "SELECT name FROM tbl_users WHERE id = " . $row_session['last_edited_by'] . " LIMIT 1;";
+                        $result_users = mysqli_query($conn, $sql_users);
+                        $row_users = mysqli_fetch_assoc($result_users);
+                        echo "<td>" . $row_users['name'] . "</td>";
+                    } else {
+                        echo "<td>-</td>";
+                    }    
+                    ?>
+                </tr>
+                
+                <tr>
+                    <th>Action</th>
+                    <?php
+                    echo "<td>";
+                    echo "<form method=\"post\">";
+                    if (isset($row_session['end_time'])){
+                        echo "<input type=\"submit\" name=\"restart-session-btn\" value=\"Restart Session\">";
+                    }
+                    else{
+                        echo "<input type=\"submit\" name=\"end-session-btn\"   value=\"End Session\">";
+                    }
+                    echo "</form>";
+                    echo "</td>";
+                    ?>                     
+                </tr>
+                
+            </thead>
         </table>
     </div>
 </div>
