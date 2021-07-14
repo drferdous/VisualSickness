9f4c8031c17e73d99985d7bf536af5e59a77747b
Kyla <57500265+kyla0509@users.noreply.github.com>
2021-07-14 15:14:06 -0400

    Study details displays information vertically in a column

--- study_details.php
+++ study_details.php
@@ -46,78 +46,100 @@ if (isset($_POST['leave-btn'])){
     <div class="card-body pr-2 pl-2">
         <table class="table table-striped table-bordered">
             <thead class="text-center">
+                <?php
+                    $sql_study = "SELECT * FROM Study WHERE study_ID = " . $_GET["study_ID"] . " LIMIT 1;";
+                    $result_study = mysqli_query($conn, $sql_study);
+                    $row_study = mysqli_fetch_assoc($result_study);
+                ?>
+                
                 <tr>
                     <th>Full Name</th>
+                    <?php
+                        echo "<td>" . $row_study['full_name']  . "</td>";     
+                    ?>    
+                </tr>    
+                    
+                <tr>        
                     <th>Short Name</th>
+                    <?php
+                        echo "<td>" . $row_study['short_name']  . "</td>";     
+                    ?>                        
+                </tr> 
+                
+                <tr>    
                     <th>IRB</th>
+                    <?php
+                        echo "<td>" . $row_study['IRB']  . "</td>";     
+                    ?>                        
+                </tr> 
+                    
+                <tr>        
                     <th>Created By</th>
+                    <?php
+                    // show name for created_by, not id                  
+                    if (isset($row_study['created_by'])){
+                        $sql_users = "SELECT name FROM tbl_users WHERE id = " . $row_study['created_by'] . " LIMIT 1;";
+                        $result_users = mysqli_query($conn, $sql_users);
+                        $row_users = mysqli_fetch_assoc($result_users);
+                            
+                        echo "<td>" . $row_users['name'] . "</td>";
+                    }
+                    else{
+                        echo "<td>-</td>";
+                    }  
+                    ?>
+                </tr> 
+                    
+                <tr>                    
                     <th>Time Created</th>
+                    <?php
+                    echo "<td>" . $row_study['created_at']     . "</td>";  
+                    ?>
+                </tr> 
+                    
+                <tr>                    
                     <th>Time of Last Edit</th>
+                    <?php
+                    echo "<td>" . $row_study['last_edited_at']     . "</td>";  
+                    ?>                    
+                </tr> 
+                    
+                <tr>
                     <th>Last Edited By</th>
+                    <?php          
+                    // show name for last_edited_by, not id    
+                    if (isset($row_study['last_edited_by'])){
+                        $sql_users = "SELECT name FROM tbl_users WHERE id = " . $row_study['last_edited_by'] . " LIMIT 1;";
+                        $result_users = mysqli_query($conn, $sql_users);
+                        $row_users = mysqli_fetch_assoc($result_users);
+                        echo "<td>" . $row_users['name'] . "</td>";
+                    }
+                    else{
+                        echo "<td>-</td>";
+                    }    
+                    ?>
+                </tr>                     
+                    
+                <tr>
                     <th>Action</th>
+                    <?php       
+                        if (Session::get('roleid') === '1' || Session::get('roleid') === '2') {
+ 
+                            echo "<td>";                             
+                            echo "<form method=\"post\">";
+                            echo "<input type=\"submit\" name=\"deactivate-btn\" value=\"Deactivate\" />";
+                            echo "</form>";
+                                
+                            echo "<a href=\"edit_study.php?id=". $row_study['study_ID'] ."\">Edit</a>";
+                        
+                        } else if (Session::get('roleid') === '3' || Session::get('roleid') === '4') {
+                            echo "<form method=\"post\">";
+                            echo "<input type=\"submit\" name=\"leave-btn\" value=\"Leave\" />";
+                            echo "</form>";
+                        }
+                    ?>                    
                 </tr>
             </thead>
-            
-            <tbody>
-                <tr>
-                <?php
-                $sql_study = "SELECT * FROM Study WHERE study_ID = " . $_GET["study_ID"] . " LIMIT 1;";
-                $result_study = mysqli_query($conn, $sql_study);
-                $row_study = mysqli_fetch_assoc($result_study);
-                echo "<td>" . $row_study['full_name']  . "</td>";
-                echo "<td>" . $row_study['short_name'] . "</td>";
-                echo "<td>" . $row_study['IRB']        . "</td>";
-                    
-                if (isset($row_study['created_by'])){
-                    $sql_users = "SELECT name FROM tbl_users WHERE id = " . $row_study['created_by'] . " LIMIT 1;";
-                    $result_users = mysqli_query($conn, $sql_users);
-                    $row_users = mysqli_fetch_assoc($result_users);
-                        
-                    echo "<td>" . $row_users['name'] . "</td>";
-                }
-                else{
-                    echo "<td>-</td>";
-                }
-                    
-                echo "<td>" . $row_study['created_at']     . "</td>";
-                echo "<td>" . $row_study['last_edited_at'] . "</td>";
-                    
-                if (isset($row_study['last_edited_by'])){
-                    $sql_users = "SELECT name FROM tbl_users WHERE id = " . $row_study['last_edited_by'] . " LIMIT 1;";
-                    $result_users = mysqli_query($conn, $sql_users);
-                    $row_users = mysqli_fetch_assoc($result_users);
-                    echo "<td>" . $row_users['name'] . "</td>";
-                }
-                else{
-                    echo "<td>-</td>";
-                }
-                    
-                echo "<td>";
-                if (Session::get('roleid') === '1' || Session::get('roleid') === '2'){
-                    echo "<ul>";
-                        
-                    echo "<li>";
-                    echo "<form method=\"post\">";
-                    echo "<input type=\"submit\" name=\"deactivate-btn\" value=\"Deactivate\" />";
-                    echo "</form>";
-                    echo "</li>";
-                        
-                    echo "<li>";
-                    echo "<a href=\"edit_study.php?id=". $row_study['study_ID'] ."\">Edit</a>";
-                    echo "</li>";
-                    
-                    echo "</ul>";
-                }
-                else if (Session::get('roleid') === '3' || Session::get('roleid') === '4'){
-                    echo "<form method=\"post\">";
-                    echo "<input type=\"submit\" name=\"leave-btn\" value=\"Leave\" />";
-                    echo "</form>";
-                }
-                echo "</td>";
-                ?>
-                </tr>
-            </tbody>
-            
         </table>
     </div>
 </div>
