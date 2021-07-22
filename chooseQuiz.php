5371eb1791b52db72465bf684d42e299758f39f3
AmazingAndrewM1 <michaea3@tcnj.edu>
2021-07-22 06:13:57 -0400

    When choosing a quiz, no information regarding the session ID is visible in the URL.

--- chooseQuiz.php
+++ chooseQuiz.php
@@ -3,23 +3,48 @@
     include 'database.php';
     
     Session::CheckSession();
-    Session::set('session_ID', intval($_GET['session_ID']));
+    Session::set('session_ID', intval($_POST['session_ID']));
     
     if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['take-ssq-btn'])) {
-        if (isset($_POST['quiz_type']) && isset($_POST['ssq_time'])){
-            if ($_POST['quiz_type'] === '0'){  // 0 represents textual quiz
-                header("Location: TextQuiz.php?session_ID=" . Session::get('session_ID') . "&ssq_time=" . $_POST['ssq_time']);
-            }
-            else if ($_POST['quiz_type'] === '1'){ // 1 represents visual quiz
-                header("Location: VisualQuiz.php?session_ID=" . Session::get('session_ID') . "&ssq_time=" . $_POST['ssq_time']);
-            }
-            else{
-                $errorMessage = '<div class="alert alert-danger alert-dismissible mt-3" id="flash-msg">
-                                <a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>
-                                <strong>Error!</strong> Invalid quiz type!</div>';
-                echo $errorMessage;
-            }
-        }
+        if (isset($_POST['quiz_type']) && isset($_POST['ssq_time'])){ ?>
+            <script type="text/javascript">
+                let form = document.createElement("form");
+                let hiddenInput;
+                let sessionID = <?php echo Session::get('session_ID'); ?>;
+                let quizType = <?php echo $_POST['quiz_type']?>;
+                let ssqTime = <?php echo $_POST['ssq_time']?>;
+                let targetURL;
+                
+                if (quizType === 0){ // 0 represents textual quiz
+                    targetURL = "TextQuiz.php";
+                }
+                else if (quizType === 1){ // 1 represents visual quiz
+                    targetURL = "VisualQuiz.php";
+                }
+                else{
+                    targetURL = "404.php";
+                }
+                
+                form.setAttribute("method", "POST");
+                form.setAttribute("action", targetURL);
+                form.setAttribute("style", "display: none");
+                
+                hiddenInput = document.createElement("input");
+                hiddenInput.setAttribute("type", "hidden");
+                hiddenInput.setAttribute("name", "session_ID");
+                hiddenInput.setAttribute("value", sessionID);
+                form.appendChild(hiddenInput);
+                
+                hiddenInput = document.createElement("input");
+                hiddenInput.setAttribute("type", "hidden");
+                hiddenInput.setAttribute("name", "ssq_time");
+                hiddenInput.setAttribute("value", ssqTime);
+                form.appendChild(hiddenInput);
+                
+                document.body.appendChild(form);
+                form.submit();
+            </script>
+    <?php }
         else{
             $errorMessage = '<div class="alert alert-danger alert-dismissible mt-3" id="flash-msg">
                             <a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>
@@ -45,7 +70,7 @@
     
     <div class="card-body pr-2 pl-2">
         <h2 class="text-center">Quiz Settings</h2>
-        <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]) . "?session_ID=" . Session::get('session_ID'); ?>" method="post">
+        <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
             <div class="form-group pt-3">
                 <label for="quiz_type">Quiz Type</label>
                 <select class="form-control" name="quiz_type" id="quiz_type">
@@ -75,6 +100,9 @@
                         }
                     ?>
                 </select>
+            </div>
+            <div>
+                <input type="hidden" name="session_ID" value="<?php Session::get('session_ID'); ?>">
             </div>
             <div class="form-group">
                 <button type="submit" name="take-ssq-btn" class="btn btn-success">Take SSQ</button>
