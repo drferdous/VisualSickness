1f5f96ebd7bea3d5d23034a3cb5a206ec523b15f
Kyla <57500265+kyla0509@users.noreply.github.com>
2021-07-22 19:28:46 -0400

    Added 404 pag

---
+++ 404.php
@@ -0,0 +1,11 @@
+<?php 
+include 'inc/header.php';
+?>
+
+<div class="card ">
+    <div class="card-header">
+        <div class="card-body pr-2 pl-2">
+            <h3>404: Page Not Found</h3>
+        </div>
+    </div>
+</div>
