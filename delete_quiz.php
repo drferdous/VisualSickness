cf848723484b3b14c1ed7fe68a2886675d5e852f
Kyla <57500265+kyla0509@users.noreply.github.com>
2021-07-29 18:27:22 -0400

    Added functionality to delete visual/text quiz

---
+++ delete_quiz.php
@@ -0,0 +1,18 @@
+<?php
+include 'inc/header.php';
+include 'database.php';
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['deleteQuiz'])) {
+  $deleteQuiz = $users->deleteQuiz($_POST);
+}
+
+if (isset($deleteQuiz)) {
+  echo $deleteQuiz;
+}
+
+?>
+
+<form action="session_details" method="post">
+        <input type="hidden" name="session_ID" value="<?php echo Session::get('session_ID'); ?>">
+        <button type="Submit" name="ok-btn" class="btn btn-success form-group">OK</button>
+</form>
