9624c0a7950c8ae6b79db89bff34da645947ff70
AmazingAndrewM1 <michaea3@tcnj.edu>
2021-11-22 10:53:26 -0500

    The login page should now redirect users to the study list page if and only if the user logs in successfully.

--- login.php
+++ login.php
@@ -3,52 +3,48 @@ include 'inc/header.php';
 Session::CheckLogin();
 ?>
 
-
-<?php
-
+<?php 
 if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['login'])) {
-   $userLog = $users->userLoginAuthentication($_POST);
-}
-if (isset($userLog)) {
-  echo $userLog;
+    $userLog = $users->userLoginAuthentication($_POST);
+    if (isset($userLog)){
+        echo $userLog;
+    }
+    if (Session::get('login') === TRUE){
+        header("Location: view_study");
+        exit();
+    }
 }
 
 $logout = Session::get('logout');
 if (isset($logout)) {
   echo $logout;
 }
-
-
-
- ?>
+?>
 
 <div class="card ">
-  <div class="card-header">
-          <h3 class='text-center'><i class="fas fa-sign-in-alt mr-2"></i>User login</h3>
+   <div class="card-header">
+          <h3 class="text-center font-weight-bold">VisualSickness</h3>
+    </div>
+    <div class="card-body">
+        <div style="width:450px; margin:0px auto; border-radius: 25px;" class="shadow">
+            <form class="" action="" method="post" style="margin:10px 10px 10px 10px; padding: 30px 0px 0px 0px;">
+                <p class="d-flex justify-content-center"><i class="fas fa-sign-in-alt mr-2"></i>Login to VisualSickness</p>
+                <div class="form-group">
+                    <label for="email">Email address</label>
+                    <input type="email" name="email" id="email" placeholder="Enter email..." class="form-control">
+                </div>
+                <div class="form-group">
+                    <label for="password">Password</label>
+                    <input type="password" name="password" id="password" placeholder="Enter password..." class="form-control">
+                </div>
+                <div class="form-group d-flex justify-content-center">
+                    <button type="submit" name="login" class="btn btn-success">Login</button>
+                </div>
+                <a href="forgot_password" class="d-flex justify-content-center">Forgot your Password?</a>
+            </form>
         </div>
-        <div class="card-body">
-            <div style="width:450px; margin:0px auto">
-                <form class="" action="" method="post">
-                    <div class="form-group">
-                        <label for="email">Email address</label>
-                        <input type="email" name="email" placeholder="Enter email..." class="form-control">
-                    </div>
-                    <div class="form-group">
-                        <label for="password">Password</label>
-                        <input type="password" name="password" placeholder="Enter password..." class="form-control">
-                    </div>
-                    <div class="form-group">
-                        <button type="submit" name="login" class="btn btn-success">Login</button>
-                    </div>
-                    <a href="forgot_password">Forgot your Password?</a>
-                </form>
-          </div>
-        </div>
-      </div>
-
-
-
-  <?php
+    </div>
+</div>
+<?php
   include 'inc/footer.php';
-
-  ?>
+?>
