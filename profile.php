292b8512d205799e6169f4c50d07216c239958aa
AmazingAndrewM1 <michaea3@tcnj.edu>
2022-01-17 09:25:02 -0500

    The profile now displays the affiliation name of the user in the profile page.

--- profile.php
+++ profile.php
@@ -1,54 +1,58 @@
 <?php
 include 'inc/header.php';
+include 'database.php';
 Session::CheckSession();
 
 $userid = $_POST['user_ID'];
 $purpose = $_POST['purpose'];
 
 if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['update'])) {
-  $updateUser = $users->updateUserByIdInfo($userid, $_POST);
-
+    $updateUser = $users->updateUserByIdInfo($userid, $_POST);
+    if (isset($updateUser)) {
+        echo $updateUser;
+    }
 }
-if (isset($updateUser)) {
-  echo $updateUser;
-}
+?>
 
-
-
-
- ?>
-
- <div class="card ">
-   <div class="card-header">
-          <h3>User Profile <span class="float-right"> <a href="index" class="btn btn-primary" data-user_ID="0">Back</a> </h3>
-        </div>
-        <div class="card-body">
-
-    <?php
-    $getUinfo = $users->getUserInfoById($userid);
-    if ($getUinfo) {
-     ?>
-
-
-          <div style="width:600px; margin:0px auto">
-
-          <form class="" action="" method="POST">
-              <div class="form-group">
-                <label for="name">Your name</label>
-                <input type="text" name="name" value="<?php echo $getUinfo->name; ?>" class="form-control">
-              </div>
-              <div class="form-group">
-                <label for="username">Your username</label>
-                <input type="text" name="username" value="<?php echo $getUinfo->username; ?>" class="form-control">
-              </div>
-              <div class="form-group">
-                <label for="email">Email address</label>
-                <input type="email" id="email" name="email" value="<?php echo $getUinfo->email; ?>" class="form-control">
-              </div>
-              <div class="form-group">
-                <label for="mobile">Mobile Number</label>
-                <input type="text" id="mobile" name="mobile" value="<?php echo $getUinfo->mobile; ?>" class="form-control">
-              </div>
+<div class="card">
+    <div class="card-header">
+        <h3>User Profile <span class="float-right"><a href="index" class="btn btn-primary" data-user_ID="0">Back</a></span></h3>
+    </div>
+    <div class="card-body">
+        <?php
+            $getUinfo = $users->getUserInfoById($userid);
+            if ($getUinfo !== FALSE){ ?>
+                <div style="width:600px; margin:0px auto">
+                <form class="" action="" method="POST">
+                <div class="form-group">
+                    <label for="name">Your Name</label>
+                    <input type="text" name="name" value="<?php echo $getUinfo->name; ?>" class="form-control">
+                </div>
+                <div class="form-group">
+                    <label for="username">Your Username</label>
+                    <input type="text" name="username" value="<?php echo $getUinfo->username; ?>" class="form-control">
+                </div>
+                <div class="form-group">
+                    <label for="email">Email Address</label>
+                    <input type="email" id="email" name="email" value="<?php echo $getUinfo->email; ?>" class="form-control">
+                </div>
+                <div class="form-group">
+                    <label for="mobile">Mobile Number</label>
+                    <input type="text" id="mobile" name="mobile" value="<?php echo $getUinfo->mobile; ?>" class="form-control">
+                </div>
+                <?php $sql = "SELECT Name
+                              FROM Affiliation
+                              WHERE id = " . $getUinfo->affiliationid . "  
+                              LIMIT 1;";
+                      $result = mysqli_query($conn, $sql);
+                      $row = mysqli_fetch_assoc($result);
+                      if (!isset($row)){
+                          $row["Name"] = "-";
+                      } ?>
+                <div class="form-group">
+                    <label for="affilation">Affilation Name</label>
+                    <input type="text" id="affiliation" name="affiliation" value="<?php echo $row["Name"]; ?>" class="form-control">
+                </div>
 
               <?php if (Session::get("roleid") == '1' && $purpose === "edit") { ?>
 
@@ -113,7 +117,6 @@ if (isset($updateUser)) {
 
               <div class="form-group">
                 <button type="submit" name="update" class="btn btn-success">Update</button>
-
               </div>
 
               <?php } ?>
