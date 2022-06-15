dbc5116503159a5164f1c1f204704712ad643faa
AmazingAndrewM1 <michaea3@tcnj.edu>
2022-06-15 11:21:19 -0400

    participantList.php is now updated to function with the encrypted study_ID.

--- participantList.php
+++ participantList.php
@@ -3,26 +3,31 @@
     include_once "lib/Database.php";
     $db = Database::getInstance();
     $pdo = $db->pdo;
-    if (isset($_POST['study_ID'])) $study_ID = $_POST['study_ID'];
+    if (isset($_GET['forStudy']) && $_GET['forStudy'] && Session::get('study_ID') != 0) $study_ID = Session::get('study_ID');
 ?>
 
 <div class="card">
     <div class="card-header">
         <h3>Participant List
-            <?php
-            if (isset($study_ID)) {
-                echo "<span class='float-right'><a href='study_details' class='btn btn-primary redirectUser' data-study_ID=" . $study_ID . ">Back</a></span>";
-            }?>
+            <?php if (isset($study_ID)) { ?>
+                <span class='float-right'>
+                    <a href='study_details' 
+                       class='btn btn-primary redirectUser'> 
+                       Back
+                    </a>
+                </span>
+            <?php } ?>
         </h3>
     </div>
     <?php
     
         $sql = "SELECT P.anonymous_name, P.dob, P.email, P.phone_no, S.full_name
-                FROM Participants AS P JOIN Study AS S ON (P.study_id = S.study_ID)
-                WHERE P.is_active AND P.study_id IN (SELECT study_ID
-                                FROM Researcher_Study
-                                WHERE researcher_ID = " . Session::get("id") . (isset($study_ID) ? " AND study_ID = $study_ID" : "") . ");";
-                
+                FROM Participants AS P 
+                JOIN Study AS S ON (P.study_id = S.study_ID)
+                WHERE P.is_active 
+                AND P.study_id IN (SELECT study_ID
+                                   FROM Researcher_Study
+                                   WHERE researcher_ID = " . Session::get("id") . (isset($study_ID) ? " AND study_ID = $study_ID" : "") . ");";
         $result = $pdo->query($sql);
         
         if (!$result){
@@ -43,7 +48,9 @@
                 </tr>
             </thead>
             <tbody>
-                <?php while ($row = $result->fetch(PDO::FETCH_ASSOC)){ ?>
+                <?php 
+                if ($result->rowCount() > 0) {
+                    while ($row = $result->fetch(PDO::FETCH_ASSOC)){ ?>
                         <tr>
                             <td><?php echo $row["anonymous_name"]; ?></td>
                             <td><?php echo $row["dob"]; ?></td>
@@ -51,6 +58,11 @@
                             <td><?php echo $row["phone_no"]; ?></td>
                             <td><?php echo $row["full_name"]; ?></td>
                         </tr>
+                <?php }
+                } else { ?>
+                    <tr>
+                        <td colspan='100%' class="text-center notFound">No participants found for this study</td>
+                    </tr>
                 <?php } ?>
             </tbody>
         </table>
@@ -58,27 +70,8 @@
 </div>
 <script>
     $(document).ready(() => {
-        $(document).on("click", "a.redirectUser", redirectUser);
+        if (!document.querySelector('.notFound')) $('#example').DataTable();
     });
-    
-    function redirectUser(){
-        let form = document.createElement("form");
-        let hiddenInput = document.createElement("input");
-        
-        form.setAttribute("method", "POST");
-        form.setAttribute("action", $(this).attr("href"));
-        form.setAttribute("style", "display: none");
-        
-        hiddenInput.setAttribute("type", "hidden");
-        hiddenInput.setAttribute("name", "study_ID");
-        hiddenInput.setAttribute("value", $(this).attr("data-study_ID"));
-        
-        form.appendChild(hiddenInput);
-        document.body.appendChild(form);
-        form.submit();
-        
-        return false;
-    }
 </script>
 <?php
   include "inc/footer.php";
