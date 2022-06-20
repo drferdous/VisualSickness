85fe658871c8d9176cdfeb75e8a65cdebf7ccb58
Paula <arroyap1@tcj.edu>
2022-06-20 16:34:02 -0400

    Added new files

---
+++ edit_researchers.php
@@ -0,0 +1,113 @@
+<?php
+include 'inc/header.php';
+include_once 'lib/Database.php';
+Session::CheckSession();
+$db = Database::getInstance();
+$pdo = $db->pdo;
+
+if (Session::get("study_ID") == 0){
+    header("Location: view_study");
+    exit();
+}
+Session::requirePI(Session::get('study_ID'), $pdo);
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['editResearcher'])) {
+    $info = explode(';', $_POST['researcher_ID']);
+    $researcher_ID = $info[0];
+    $iv = $info[1];
+    $researcher_ID = Crypto::decrypt($researcher_ID, hex2bin($iv));
+    $editResearcher = $studies->editResearcher($researcher_ID, $_POST['study_role']);
+}
+
+if (isset($editResearcher)) {
+    echo $editResearcher;?>
+    <script type="text/javascript">
+        const divMsg = document.getElementById("flash-msg");
+        if (divMsg.classList.contains("alert-success")){
+            setTimeout(function(){
+                let redirectURL = "study_details";
+                location.href = redirectURL;
+            }, 1000);
+        }
+    </script>
+<?php } ?>
+
+<div class="card">
+    <div class="card-header">
+        <h3 class="float-left">Edit A Researcher</h3>
+        <a href="study_details" 
+           class="btn btn-primary float-right redirectUser">
+           Back
+        </a>
+    </div>
+    <div class="card-body pr-2 pl-2">
+        <form class="" action="" method="post">
+            <div style="margin-block: 6px;">
+                <small style='color: red'>
+                    * Required Field
+                </small>
+            </div>
+            <div class="form-group">
+                <div class="form-group">
+                    <label for="researcher_ID" class="required">Edit A Researcher</label>
+                    <?php
+                    $sql = "SELECT id, name, email
+                            FROM tbl_users
+                            WHERE id IN (SELECT researcher_ID 
+                                             FROM Researcher_Study
+                                             WHERE study_ID = " . Session::get("study_ID") . 
+                                             " AND is_active = 1 AND study_role != 2)
+                            AND status = 1
+                            AND affiliationid = " . Session::get("affiliationid") . ";";
+                    $result = $pdo->query($sql); ?>
+                    <select class="form-control" name="researcher_ID" id="researcher_ID" required <?= $result->rowCount() === 0 ? 'disabled' : '' ?>>
+                        <option value="" disabled hidden selected><?= $result->rowCount() === 0 ? 'There are no other researchers in this study!' : 'Researcher Name' ?></option>
+                        <?php
+                        while ($row = $result->fetch(PDO::FETCH_ASSOC)){
+                                $enc_id = Crypto::encrypt($row['id'], $iv); ?>
+                                <option value="<?= $enc_id ?>;<?= bin2hex($iv) ?>"><?= $row["name"] . " (" . $row["email"] . ")" ?></option>
+                        <?php } ?>
+                    </select>
+                    <br>
+                    <label for="study_role" class="required">Select Study Role</label>
+                    <select class="form-control" name="study_role" id="study_role" required disabled>
+                        <option value="" selected hidden disabled>Study Role</option>
+                    </select> 
+                </div>
+            </div>
+            <?php if ($result->rowCount()) { ?>
+                <div class="form-group">
+                    <button type="submit" name="editResearcher" class="btn btn-success">Submit</button>
+                </div>
+            <?php } ?>
+        </form>
+    </div>
+</div>
+      
+<script type="text/javascript">
+     $(document).ready(function() {
+         $('#researcher_ID').change(function() {
+            const info = $(this).val();
+            const researcher_ID = info.split(';')[0];
+            const iv = info.split(';')[1];
+            $.ajax({
+                url: "researcherrole",
+                type: "POST",
+                cache: false,
+                data: {
+                    researcher_ID,
+                    iv
+                },
+                success:function(data){
+                    let studyRoleSelector = document.getElementById("study_role");
+                    $(studyRoleSelector).html(data);
+                    studyRoleSelector.removeAttribute("disabled");
+                }
+            });	
+         });
+     });
+ </script>
+
+<?php
+  include 'inc/footer.php';
+?>
---
+++ participant_more_info.php
@@ -0,0 +1,126 @@
+<?php
+    include "inc/header.php";
+    
+    $db = Database::getInstance();
+    $pdo = $db->pdo;
+    
+    $iv = hex2bin($_POST["iv"]);
+    $participant_ID = Crypto::decrypt($_POST["participant_ID"], $iv);
+    
+    $sql = "SELECT * FROM Participants AS P
+            INNER JOIN Demographics AS D
+            ON P.demographics_id = D.ID
+            WHERE participant_ID = " . $participant_ID . "
+            LIMIT 1;";
+    $result = $pdo->query($sql);
+    $row = $result->fetch(PDO::FETCH_ASSOC);
+    
+    Session::requireResearcherOrUser(intval($row['study_id']), $pdo);
+    
+    if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['removeParticipant'])) {
+        $removeParticipant = $studies->removeParticipant($participant_ID);
+    
+        if (isset($removeParticipant)) {
+            echo $removeParticipant;?>
+            <script type="text/javascript">
+                const divMsg = document.getElementById("flash-msg");
+                if (divMsg.classList.contains("alert-success")){
+                    setTimeout(function(){
+                        let redirectURL = "participantList?<?= isset($_POST['forStudy']) ? 'forStudy=true' : '' ?>";
+                        location.href = redirectURL;
+                    }, 1000);
+                }
+            </script>
+        <?php }
+    }
+    
+    $study_sql = "SELECT full_name FROM Study
+                    WHERE study_ID = " . $row['study_id'] . " LIMIT 1;";
+    $study_name_result = $pdo->query($study_sql);
+    $study_name = $study_name_result->fetch(PDO::FETCH_ASSOC)['full_name'];
+    
+    $iv = hex2bin($row["iv"]);
+    $name = Crypto::decrypt($row["anonymous_name"], $iv); ?>
+<div class="card">
+    <div class="card-header">
+        <span class="float-left"><h3><?= $name ?></h3></span>
+        <span class="float-right">
+            <a href="participantList?<?= isset($_POST['forStudy']) ? 'forStudy=true' : '' ?>"
+               class="btn btn-primary">
+               Back
+            </a>
+        </span>
+    </div>
+    <div class="card-body">
+        <div style="overflow-x: auto">
+            <table class="table table-striped table-bordered table-responsive" style="display: table; max-width: 600px; margin: 0px auto;">
+                <tbody>
+                    <tr>
+                        <th>DOB</th>
+                        <td><?= $row["dob"] ?></td>
+                    </tr>
+                    <tr>
+                        <th>Age</th>
+                        <td><?= $row["Age"] ?></td>
+                    </tr>
+                    <tr>
+                        <th>Weight</th>
+                        <td><?= $row["weight"] ?></td>
+                    </tr>
+                    <tr>
+                        <th>Gender</th>
+                        <td><?= $row["Gender"] ?></td>
+                    </tr>
+                    <tr>
+                        <th>Education</th>
+                        <td><?= $row["Education"] ?></td>
+                    </tr>
+                    <tr>
+                        <th>Race Ethnicity</th>
+                        <td><?= $row["Race_Ethnicity"] ?></td>
+                    </tr>
+                    <tr>
+                        <th>Occupation</th>
+                        <td><?= $row["occupation"] ?></td>
+                    </tr>
+                    <tr>
+                        <th>Phone Number</th>
+                        <td><a href="tel:<?= $row['phone_no'] ?>"><?= $row["phone_no"] ?></a></td>
+                    </tr>
+                    <tr>
+                        <th>Email</th>
+                        <td><a href="mailto:<?= $row['email'] ?>"><?= $row["email"] ?></a></td>
+                    </tr>
+                    <tr>
+                        <th>Comments</th>
+                        <td><?= $row["comments"] ?></td>
+                    </tr>
+                </tbody>
+            </table>
+            <?php
+            $role_sql = "SELECT study_role FROM Researcher_Study
+                        WHERE researcher_ID = " . Session::get('id') . "
+                        AND study_ID = " . $row['study_id'] . "
+                        AND is_active = 1
+                        LIMIT 1;";
+            $role_result = $pdo->query($role_sql);
+            $role_row = $role_result->fetch(PDO::FETCH_ASSOC);
+            if (!isset($role_row['study_role'])) {
+                header('Location: view_study');
+                exit();
+            }
+            if ($role_row['study_role'] == 2) { ?>
+            <form class="text-center mt-2" action="" method="POST" onsubmit="return confirm('Are you sure you want to remove <?= $name ?> from the study \'<?= $study_name ?>\'? This action cannot be undone.');">
+                <input type="submit" name="removeParticipant" class="btn btn-danger" value="Remove <?= $name ?> from '<?= $study_name ?>'">
+                <input name="participant_ID" type="hidden" value="<?= $_POST['participant_ID'] ?> ?>">
+                <input name="iv" type="hidden" value="<?= $_POST['iv'] ?>">
+            </form>
+            <?php } ?>
+        </div>
+    </div>
+</div>
+
+
+<?php
+    include "inc/footer.php";
+?>
