a9453807a4587b9947428dfec7ba4942689990a1
xaridar <xaridara@gmail.com>
2022-07-06 13:59:02 -0400

    styling

--- report_options.php
+++ report_options.php
@@ -14,63 +14,68 @@
 
     if (isset($_POST['study_id']) && !empty($_POST['study_id']) && isset($_POST['iv'])) {
         $id = Crypto::decrypt($_POST['study_id'], hex2bin($_POST['iv'])); ?>
+        <div class="form-group">
+            <label for="session_id">Sessions</label>
+            <?php 
+                $sql = "SELECT name, id
+                        FROM session_times WHERE study_id = " . $id . " AND is_active = 1";
+                $result = $pdo->query($sql); ?>
+            <select class="form-control form-select" name="session_id" id="session_id">
 
-        <label for="session_id">Sessions</label>
-        <?php 
-            $sql = "SELECT name, id
-                    FROM session_times WHERE study_id = " . $id . " AND is_active = 1";
-            $result = $pdo->query($sql); ?>
-        <select class="form-control form-select" name="session_id" id="session_id">
-
-        <?php if($result->rowCount() === 0) { ?>
-                <option value="" disabled hidden selected>There are no sessions available!</option>
-        <?php } else {?>
-            <option value="" selected>All Sessions</option>
-        <?php } 
-            while ($row = $result->fetch(PDO::FETCH_ASSOC)) { 
-                $enc_id = Crypto::encrypt($row['id'], $iv); ?>
-                <option value="<?= $enc_id ?>;<?= bin2hex($iv) ?>"><?php echo $row['name'];?></option>
-            <?php } ?>
-        </select>
+            <?php if($result->rowCount() === 0) { ?>
+                    <option value="" disabled hidden selected>There are no sessions available!</option>
+            <?php } else {?>
+                <option value="" selected>All Sessions</option>
+            <?php } 
+                while ($row = $result->fetch(PDO::FETCH_ASSOC)) { 
+                    $enc_id = Crypto::encrypt($row['id'], $iv); ?>
+                    <option value="<?= $enc_id ?>;<?= bin2hex($iv) ?>"><?php echo $row['name'];?></option>
+                <?php } ?>
+            </select> 
+        </div>
         
-        <label for="participant_id">Participants</label>
-        <?php 
-            $sql = "SELECT anonymous_name, iv, dob, participant_id 
-                    FROM participants 
-                    WHERE is_active = 1 AND study_id = 97";
-            $result = $pdo->query($sql); ?>
-        <select class="form-control form-select" name="participant_id" id="participant_id">
+        <div class="form-group">
+            <label for="participant_id">Participants</label>
+            <?php 
+                $sql = "SELECT anonymous_name, iv, dob, participant_id 
+                        FROM participants 
+                        WHERE is_active = 1 AND study_id = 97";
+                $result = $pdo->query($sql); ?>
+            <select class="form-control form-select" name="participant_id" id="participant_id">
 
-        <?php if($result->rowCount() === 0) { ?>
-                <option value="" disabled hidden selected>There are no participants available!</option>
-        <?php } else {?>
-            <option value="" selected>All Participants</option>
-        <?php } 
-            while ($row = $result->fetch(PDO::FETCH_ASSOC)) {  
-                $enc_id = Crypto::encrypt($row['participant_id'], $iv_participant);
-                $iv = hex2bin($row['iv']);
-                $name = Crypto::decrypt($row['anonymous_name'], $iv); ?>
-                <option value="<?= $enc_id ?>;<?= bin2hex($iv_participant) ?>"><?php echo $name . " - " . $row['dob']; ?></option>
-            <?php } ?>
-        </select>
+            <?php if($result->rowCount() === 0) { ?>
+                    <option value="" disabled hidden selected>There are no participants available!</option>
+            <?php } else {?>
+                <option value="" selected>All Participants</option>
+            <?php } 
+                while ($row = $result->fetch(PDO::FETCH_ASSOC)) {  
+                    $enc_id = Crypto::encrypt($row['participant_id'], $iv_participant);
+                    $iv = hex2bin($row['iv']);
+                    $name = Crypto::decrypt($row['anonymous_name'], $iv); ?>
+                    <option value="<?= $enc_id ?>;<?= bin2hex($iv_participant) ?>"><?php echo $name . " - " . $row['dob']; ?></option>
+                <?php } ?>
+            </select> 
+        </div>
 
-        <label for="SSQ_id">SSQ Times</label>
-        <?php 
-            $sql = "SELECT name, id
-                    FROM ssq_times 
-                    WHERE study_id = " . $id . " AND is_active = 1";
-            $result = $pdo->query($sql); ?>
-        <select class="form-control form-select" name="SSQ_id" id="SSQ_id">
-        <?php if($result->rowCount() === 0) { ?>
-                <option value="" disabled hidden selected>There are no SSQ Times available!</option>
-        <?php } else {?>
-            <option value="" selected>All SSQ Times</option>
-        <?php } 
-            while ($row = $result->fetch(PDO::FETCH_ASSOC)) {  
-                $enc_id = Crypto::encrypt($row['id'], $iv);?>
-                <option value="<?= $enc_id ?>;<?= bin2hex($iv) ?>"><?php echo $row['name'];?></option>
-            <?php } ?>
-        </select>
+        <div class="form-group">
+            <label for="SSQ_id">SSQ Times</label>
+            <?php 
+                $sql = "SELECT name, id
+                        FROM ssq_times 
+                        WHERE study_id = " . $id . " AND is_active = 1";
+                $result = $pdo->query($sql); ?>
+            <select class="form-control form-select" name="SSQ_id" id="SSQ_id">
+            <?php if($result->rowCount() === 0) { ?>
+                    <option value="" disabled hidden selected>There are no SSQ Times available!</option>
+            <?php } else {?>
+                <option value="" selected>All SSQ Times</option>
+            <?php } 
+                while ($row = $result->fetch(PDO::FETCH_ASSOC)) {  
+                    $enc_id = Crypto::encrypt($row['id'], $iv);?>
+                    <option value="<?= $enc_id ?>;<?= bin2hex($iv) ?>"><?php echo $row['name'];?></option>
+                <?php } ?>
+            </select>
+        </div>
 
 <?php }  else { ?>
     <div class="form-group">
@@ -78,15 +83,13 @@
         <select class="form-control form-select" name="session_id" id="session_id" disabled>
             <option value="" selected hidden disabled>Session</option>
         </select>
-    </div> 
-    <br>
+    </div>
     <div class="form-group">
         <label for="participant_id">Participants</label>
         <select class="form-control form-select" name="participant_id" id="participant_id" disabled>
             <option value="" selected hidden disabled>Participant</option>
         </select> 
     </div>
-    <br>
     <div class="form-group">
         <label for="SSQ_id">SSQ Times</label>
         <select class="form-control form-select" name="SSQ_id" id="SSQ_id" disabled>
