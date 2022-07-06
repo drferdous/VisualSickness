f1c44847e5d42b70eadef6f45e7efec3b636eead
Paula <arroyap1@tcnj.edu>
2022-07-06 13:49:07 -0400

    fix html format

---
+++ create_report.php
@@ -0,0 +1,138 @@
+<?php
+    include 'inc/header.php';
+    include_once 'lib/Database.php';
+    Session::CheckSession();
+    $db = Database::getInstance();
+    $pdo = $db->pdo;
+
+    if(Session::get('roleid') == 4) {
+        header('Location: study_list');
+        exit();
+    }
+
+    $idToSearch = Session::get('id');
+?>
+
+<div class="card">
+    <div class="card-header">
+        <h1 class="float-left mb-0">Create Report</h1>
+    </div>
+    <div class="card-body">
+        <form onsubmit="downloadReport">
+            <?php 
+                $rand = bin2hex(openssl_random_pseudo_bytes(16));
+                Session::set("post_ID", $rand);
+            ?>
+
+            <input type="hidden" name="randCheck" value="<?php echo $rand; ?>">
+            <div style="margin-block: 6px;">
+                <small class='required-msg'>
+                    * Required Field
+                </small>
+            </div>
+
+            <div class="form-group">
+                <div class="form-group">
+                    <label for="study_id" class="required">Studies</label>
+                    <?php 
+                        $sql = "SELECT S.full_name, S.study_id
+                               FROM study AS S JOIN researchers as R ON(S.study_id = R.study_id) 
+                               WHERE S.is_active = 1 AND R.is_active = 1 AND researcher_id = " . $idToSearch . " AND R.study_role < 4;";
+
+                        $result = $pdo->query($sql);
+                    ?>
+                    <select class="form-control form-select" name="study_id" id="study_id" <?= $result->rowCount() === 0 ? 'disabled' : '' ?>>
+                        <?php if($result->rowCount() === 0) { ?>
+                            <option value="" disabled hidden selected>There are no studies available!</option>
+                        <?php } else {?>
+                            <option value="" selected>All Studies</option>
+                        <?php }
+                            while ($row = $result->fetch(PDO::FETCH_ASSOC)){ 
+                                $enc_id = Crypto::encrypt($row['study_id'], $iv); ?>
+                                <option value="<?= $enc_id ?>;<?= bin2hex($iv) ?>"><?php echo $row['full_name'];?></option>
+                        <?php } ?>
+                    </select>
+                    <br>
+                    <div id="report_info">
+                        <label for="session_id">Sessions</label>
+                        <select class="form-control form-select" name="session_id" id="session_id" disabled>
+                            <option value="" selected hidden disabled>Session</option>
+                        </select> 
+                        <br>
+                        <label for="participant_id">Participants</label>
+                        <select class="form-control form-select" name="participant_id" id="participant_id" disabled>
+                            <option value="" selected hidden disabled>Participant</option>
+                        </select> 
+                        <br>
+                        <label for="SSQ_id">SSQ Times</label>
+                        <select class="form-control form-select" name="SSQ_id" id="SSQ_id" disabled>
+                            <option value="" selected hidden disabled>SSQ Time</option>
+                        </select> 
+                    </div>
+                </div>
+            </div>
+            <br>
+            <?php if ($result->rowCount()) { ?>
+                <div class="form-group">
+                    <button type="submit" name="create_report" class="btn btn-success">Submit</button>
+                </div>
+            <?php } ?>
+        </form>
+    </div>
+</div>
+
+<script>
+     $(document).ready(function() {
+         $('#study_id').change(function() {
+            const info = $(this).val();
+            const study_id = info.split(';')[0];
+            const iv = info.split(';')[1];
+            $.ajax({
+                url: "report_options",
+                type: "POST",
+                cache: false,
+                data: {
+                    study_id,
+                    iv
+                },
+                success:function(data){
+                    let reportSelector = document.getElementById("report_info");
+                    $(reportSelector).html(data);
+                }
+            });	
+         });
+     });
+     function downloadReport() {
+        const study_ID = $("#study_id").val().split(';')[0];
+        const study_iv = $("#study_id").val()?.split(';')[1];
+        const session_ID = $("#session_id").val().split(';')[0];
+        const session_iv = $("#session_id").val()?.split(';')[1];
+        const participant_ID = $("#participant_id").val().split(';')[0];
+        const participant_iv = $("#participant_id").val()?.split(';')[1];
+        const ssq_times_ID = $("#SSQ_id").val().split(';')[0];
+        const ssq_iv = $("#SSQ_id").val()?.split(';')[1];
+        $.ajax({
+                url: "report_download",
+                type: "POST",
+                cache: false,
+                data: {
+                    study_ID,
+                    study_iv,
+                    session_ID,
+                    session_iv,
+                    participant_ID,
+                    participant_iv,
+                    ssq_times_ID,
+                    ssq_iv,
+                    downloadResults: true
+                },
+                success:function(data){
+                    console.log(data);
+                }
+            });	
+     }
+ </script>
+
+<?php
+  include 'inc/footer.php';
+?>
---
+++ report_options.php
@@ -0,0 +1,77 @@
+<?php
+    include_once 'lib/Database.php';
+    include_once 'classes/Crypto.php';
+    include_once 'lib/Session.php';
+    if ($_SERVER["REQUEST_METHOD"] !== "POST" || !isset($_POST["study_id"])){
+        header("Location: 404");
+        exit();
+    }
+
+    Session::init();
+
+    $db = Database::getInstance();
+    $pdo = $db->pdo;
+
+    if (isset($_POST['study_id']) && !empty($_POST['study_id']) && isset($_POST['iv'])) {
+        $id = Crypto::decrypt($_POST['study_id'], hex2bin($_POST['iv'])); ?>
+
+        <label for="session_id">Sessions</label>
+        <?php 
+            $sql = "SELECT name, id
+                    FROM session_times WHERE study_id = " . $id . " AND is_active = 1";
+            $result = $pdo->query($sql); ?>
+        <select class="form-control form-select" name="session_id" id="session_id">
+
+        <?php if($result->rowCount() === 0) { ?>
+                <option value="" disabled hidden selected>There are no sessions available!</option>
+        <?php } else {?>
+            <option value="" selected>All Sessions</option>
+        <?php } 
+            while ($row = $result->fetch(PDO::FETCH_ASSOC)) { 
+                $enc_id = Crypto::encrypt($row['id'], $iv); ?>
+                <option value="<?= $enc_id ?>;<?= bin2hex($iv) ?>"><?php echo $row['name'];?></option>
+            <?php } ?>
+        </select> 
+        <br>
+        
+        <label for="participant_id">Participants</label>
+        <?php 
+            $sql = "SELECT anonymous_name, iv, dob, participant_id 
+                    FROM participants 
+                    WHERE is_active = 1 AND study_id = 97";
+            $result = $pdo->query($sql); ?>
+        <select class="form-control form-select" name="participant_id" id="participant_id">
+
+        <?php if($result->rowCount() === 0) { ?>
+                <option value="" disabled hidden selected>There are no participants available!</option>
+        <?php } else {?>
+            <option value="" selected>All Participants</option>
+        <?php } 
+            while ($row = $result->fetch(PDO::FETCH_ASSOC)) {  
+                $enc_id = Crypto::encrypt($row['participant_id'], $iv_participant);
+                $iv = hex2bin($row['iv']);
+                $name = Crypto::decrypt($row['anonymous_name'], $iv); ?>
+                <option value="<?= $enc_id ?>;<?= bin2hex($iv_participant) ?>"><?php echo $name . " - " . $row['dob']; ?></option>
+            <?php } ?>
+        </select> 
+        <br>
+
+        <label for="SSQ_id">SSQ Times</label>
+        <?php 
+            $sql = "SELECT name, id
+                    FROM ssq_times 
+                    WHERE study_id = " . $id . " AND is_active = 1";
+            $result = $pdo->query($sql); ?>
+        <select class="form-control form-select" name="SSQ_id" id="SSQ_id">
+        <?php if($result->rowCount() === 0) { ?>
+                <option value="" disabled hidden selected>There are no SSQ Times available!</option>
+        <?php } else {?>
+            <option value="" selected>All SSQ Times</option>
+        <?php } 
+            while ($row = $result->fetch(PDO::FETCH_ASSOC)) {  
+                $enc_id = Crypto::encrypt($row['id'], $iv);?>
+                <option value="<?= $enc_id ?>;<?= bin2hex($iv) ?>"><?php echo $row['name'];?></option>
+            <?php } ?>
+        </select>
+
+<?php } ?>
