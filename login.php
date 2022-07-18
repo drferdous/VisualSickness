79801fcf34fc075d1bffc5ffe90ce06ed5168f59
Paula <arroyap1@tcnj.edu>
2022-07-18 11:23:44 -0400

    Adding modified files from old visualsickness.com

--- login.php
+++ login.php
@@ -29,42 +29,114 @@ if (isset($logout)) {
           <h1 class="text-center font-weight-bold">Visual Sickness</h1>
     </div>
     <div class="card-body">
-        <div style="max-width:450px; margin:0px auto; border-radius: 25px;" class="shadow">
-            <form class="" action="<?php echo htmlspecialchars($_SERVER["REQUEST_URI"]); ?>" method="post" style="margin: 10px; padding-top: 30px;">
-                <p class="d-flex justify-content-center"><i class="fas fa-sign-in-alt mr-2"></i>Login to Visual Sickness</p>
-                <?php 
-                    $rand = bin2hex(openssl_random_pseudo_bytes(16));
-                    Session::set("post_ID", $rand);
-                ?>
-                <input type="hidden" name="randCheck" value="<?php echo $rand; ?>">
-                <div style="margin-block: 6px;">
-                    <small class='required-msg'>
-                        * Required Field
-                    </small>
-                </div>
-                <div class="form-group">
-                    <label for="email" class="required">Email address</label>
-                    <input type="email" value="<?= Util::getValueFromPost('email', $_POST); ?>" name="email" id="email" placeholder="Enter email..." class="form-control" autofocus required>
-                </div>
-                <div class="form-group">
-                    <label for="password" class="required">Password</label>
-                    <div class="input-group">
-                        <input type="password" value="<?= Util::getValueFromPost('password', $_POST); ?>" name="password" id="password" placeholder="Enter password..." data-toggle="password" class="form-control" required>
-                        <div class="input-group-append">
-                            <span class="input-group-text">
-                                <i class="fa fa-eye"></i>
-                            </span>
+        <div class="d-flex flex-wrap w-100">
+            <div style="flex: 1 1 50%" class="mb-4">
+                <h2 class="text-center mb-3">
+                    Researcher Login
+                </h2>
+                <div style="max-width:450px; margin:0px auto; border-radius: 25px;" class="shadow">
+                    <form class="" action="<?php echo htmlspecialchars($_SERVER["REQUEST_URI"]); ?>" method="post" style="margin: 10px; padding-top: 30px;">
+                        <p class="d-flex justify-content-center"><i class="fas fa-sign-in-alt mr-2"></i>Login to Visual Sickness</p>
+                        <?php 
+                            $rand = bin2hex(openssl_random_pseudo_bytes(16));
+                            Session::set("post_ID", $rand);
+                        ?>
+                        <input type="hidden" name="randCheck" value="<?php echo $rand; ?>">
+                        <div style="margin-block: 6px;">
+                            <small class='required-msg'>
+                                * Required Field
+                            </small>
                         </div>
-                    </div>
+                        <div class="form-group">
+                            <label for="email" class="required">Email address</label>
+                            <input type="email" value="<?= Util::getValueFromPost('email', $_POST); ?>" name="email" id="email" placeholder="Enter email..." class="form-control" autofocus required>
+                        </div>
+                        <div class="form-group">
+                            <label for="password" class="required">Password</label>
+                            <div class="input-group">
+                                <input type="password" value="<?= Util::getValueFromPost('password', $_POST); ?>" name="password" id="password" placeholder="Enter password..." data-toggle="password" class="form-control" required>
+                                <div class="input-group-append">
+                                    <span class="input-group-text">
+                                        <i class="fa fa-eye"></i>
+                                    </span>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="form-group d-flex justify-content-center">
+                            <button type="submit" name="login" class="btn btn-success">Login</button>
+                        </div>
+                        <a href="forgot_password" class="d-flex justify-content-center">Forgot your Password?</a>
+                    </form>
                 </div>
-                <div class="form-group d-flex justify-content-center">
-                    <button type="submit" name="login" class="btn btn-success">Login</button>
+            </div>
+            <div style="flex: 1 1 50%" class="mb-4">
+                <h2 class="text-center mb-3">
+                    Participant Registration
+                </h2>
+                <div style="max-width:450px; margin:0px auto; border-radius: 25px;" class="shadow px-4 pb-2 pt-4">
+                    <form class="" method="post" id="codeForm" onsubmit="return Validate()">
+                        <h4>Enter Your Confirmation Code</h4>
+                        <div class="form-group">
+                            <input type="text" class="form-control" pattern="^[a-zA-Z][B-DF-HJ-NP-TV-Z]\d[aeiou][235689]\d{3}$" placeholder="Registration Code" id="txtName"/>
+                        </div>
+                        <div class="form-group d-flex justify-content-center">
+                            <button type="submit" class="btn btn-success">Submit</button>
+                        </div>
+                        <p class="text-center">
+                            Not registered for the study? Click <a href="https://docs.google.com/forms/d/e/1FAIpQLScSCdtMj7uQILJpLQPhyVRZa6S5bLZlaPA1ruJ-OV_1gzb8Mw/viewform" target="_blank">
+                                here
+                            </a>
+                        </p>
+                        </div>
+                    </form>
                 </div>
-                <a href="forgot_password" class="d-flex justify-content-center">Forgot your Password?</a>
-            </form>
         </div>
     </div>
 </div>
+<script>
+    function Validate() {
+        const attr = $('#codeForm').attr('action');
+        if (typeof attr !== 'undefined' && attr !== false) {
+            return true;
+        }
+        const code = $('#txtName').val();
+        
+        $.ajax({
+            url: 'check_code.php',
+            type: 'POST',
+            cahe: false,
+            data: { userCode: code },
+            success: function(data) {
+                if(data=='exist') {
+                  alert("Code has already been used! Contact visualsicknessstudy@gmail.com for more information.")
+                  return false;
+                }
+
+                const regexCheck = /^[a-zA-Z][B-DF-HJ-NP-TV-Z]\d[aeiou][235689]\d{3}$/;
+            
+                if (!regexCheck.test(code)) {
+                    alert("Please enter a valid code");
+                    return;
+                }
+                
+                const codeForm = $("#codeForm");
+                const codeInp = document.createElement('input');
+                codeInp.setAttribute('name', 'code');
+                codeInp.setAttribute('type', 'hidden');
+                codeInp.setAttribute('value', code);
+                codeForm.append(codeInp);
+                
+                if (/[A-Z]/.test(code.charAt(0))) {
+                    codeForm.attr("action","adult_consent_form");
+                } else {
+                    codeForm.attr("action","parental_permission_form");
+                }
+                codeForm.submit();
+            }
+        });
+        return false;
+    }
+</script>
 <?php
   include 'inc/footer.php';
 ?>
