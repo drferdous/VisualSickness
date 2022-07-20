9f5065ddb52f35543b882397c3f095b4b616a193
xaridar <xaridara@gmail.com>
2022-07-20 14:39:46 -0400

    change email send to include code

--- code_insert_quiz.php
+++ code_insert_quiz.php
@@ -111,14 +111,10 @@
                     $email = $value[0][0];
                     $to          = $email;
                     $from        = "Visual Sickness Study <visualsicknessstudy@gmail.com>";
-                    $subject     = 'Thank You for Your Participation'; // email subject
-                    $body        = 'Hello,<br><br>
-                                    Thank you for your participation in the Visual Sickness study.';
+                    $subject     = "Thank You for Your Participation [$code]"; // email subject
+                    $body        = "Hello,<br><br>
+                                    You have successfully completed the study with code $code. Please be on the lookout for communication regarding compensation.";
 
-                    if(preg_match('/^[A-Z].*$/', $code)) {
-                        $body .= 'After all your children have completed the study, please forward this email to visualsickness@gmail.com to receive your compensation.';
-                    }
-                    
                     $eol = PHP_EOL;
                     $semi_rand     = md5(time());
                     $mime_boundary = "==Multipart_Boundary_x{$semi_rand}x";
